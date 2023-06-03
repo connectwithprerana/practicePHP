cc6f048bec6f0199a565e521c8df9b373b45f0bc
Prerana Sharma <connectwithprerana@gmail.com>
2023-06-03 14:06:05 +0530

    added

---
+++ echostatement.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "hello karishma";
+echo "<br>";
+
+echo "<br>12345 <BR>";
+
+echo  ('<B><I><U>hello karishma<br>');
+//----what are variable ?
+echo "yahoo baba<br>";
+echo "yahoo baba<br>";
+echo "yahoo baba<br>";
+echo "yahoo baba<br>";
+$a="yahoo baba";
+echo $a;
+$num=25.87;
+echo $num;
+?>
